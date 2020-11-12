3ee5cd9aa897ef02a1cb728eca8b7fa5c0f09343
DavidV510 <72439823+DavidV510@users.noreply.github.com>
2020-11-12 12:09:39 +0200

    Update admin-ajax.js

--- web/app/themes/Epicure/js/admin-ajax.js
+++ web/app/themes/Epicure/js/admin-ajax.js
@@ -1,20 +1,23 @@
 var $=jQuery
  
+//  if($('.striped > tbody > :nth-child(odd), ul.striped > :nth-child(odd), .alternate')){
+//     $('.striped > tbody > :nth-child(odd), ul.striped > :nth-child(odd), tr td').css('border-top','2px solid black');
+//  }
+//  $('.wisper').parent().css('display','none')
+// $('.users .column-phone').css('width','4vw');
+// $('.users .column-email').css('width','13vw')
+// $('.users .column-role').css('width','4vw');
+// if($('[data-colname="Item List"]')){
+//     var itemElements=$('[data-colname="Item List"]');
+//     var wisper=$('.wisper')
+//     for(var i=0;i<itemElements.length;i++){
+//         itemElements[i].innerHTML=wisper[i].innerHTML
+//     }
+//      $('[data-colname="Item List"]').append($('.wisper ').html())
+//     $('[data-colname="Item List"]').css('position','relative')
+// }$('#username').css('width','10rem');
 
-if($('.striped > tbody > :nth-child(odd), ul.striped > :nth-child(odd), .alternate')){
-    $('.striped > tbody > :nth-child(odd), ul.striped > :nth-child(odd), .alternate').css('display','none');
-}
 
-if($('[data-colname="Item List"]')){
-    var itemElements=$('[data-colname="Item List"]');
-    var wisper=$('.wisper')
-    for(var i=0;i<itemElements.length;i++){
-        itemElements[i].innerHTML=wisper[i].innerHTML
-    }
-    //$('[data-colname="Item List"]').append($('.wisper ').html())
-}
-
-$('#username').css('width','10rem');
 $('#name').css('width','8rem');
 $('#email').css('width','12rem');
 $('#phone').css('width','12rem');
